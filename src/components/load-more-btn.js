9361d946e40bccf84405ae4be8f82896e48e48b5
www-net <78052177+www-net@users.noreply.github.com>
2022-09-15 16:26:13 +0300

    added webpack and modules

---
+++ src/components/load-more-btn.js
@@ -0,0 +1,8 @@
+// Отрисовка кнопки "Show more"
+const createButtonTemplate = () => {
+  return (
+    `<button class="films-list__show-more">Show more</button>`
+  );
+};
+
+export { createButtonTemplate };
